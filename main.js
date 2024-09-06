cb59e95222829585e13f53e8b4bcb5e3bf04d9bb
Graeme Dyas <admin@zabouth.com>
2024-09-06 12:00:58 +0100

    Added UI
    Added ui to the game

--- main.js
+++ main.js
@@ -2,80 +2,98 @@ function getComputerChoice() {
     return ["rock", "paper", "scissors"][Math.floor(Math.random() * 3)];
 }
 
-function getHumanChoice() {
-    uc = prompt("Pick rock, paper or scissors")
-    return uc.toLowerCase();
+let humanScore = 0
+let computerScore = 0
+let result = ""
+function playRound(humanChoice, computerChoice) {
+
+    if (humanChoice == "rock") {
+        switch (computerChoice) {
+            case "rock":
+                result = ("Its a Draw " + computerChoice + " draws with " + humanChoice)
+                break;
+            case "paper":
+                result = ("You Lose " + computerChoice + " Beats " + humanChoice)
+                computerScore += 1;
+                break;
+            case "scissors":
+                result = ("You Win " + humanChoice + " Beats " + computerChoice)
+                humanScore += 1
+                break;
+            default:
+                break;
+        }
+    }
+    if (humanChoice == "paper") {
+        switch (computerChoice) {
+            case "rock":
+                result = ("You Win " + humanChoice + " Beats " + computerChoice)
+                humanScore += 1
+                break;
+            case "paper":
+                result = ("Its a Draw " + computerChoice + " draws with " + humanChoice)
+                break;
+            case "scissors":
+                result = ("You Lose " + computerChoice + " Beats " + humanChoice)
+                computerScore += 1;
+                break;
+            default:
+                break;
+        }
+
+    }
+    if (humanChoice == "scissors") {
+        switch (computerChoice) {
+            case "rock":
+                result = ("You Lose " + computerChoice + " Beats " + humanChoice)
+                computerScore += 1;
+                break;
+            case "paper":
+                result = ("You Win " + humanChoice + " Beats " + computerChoice)
+                humanScore += 1
+                break;
+            case "scissors":
+                result = ("Its a Draw " + computerChoice + " draws with " + humanChoice)
+                break;
+            default:
+                break;
+        }
+    }
 }
 
 
-function playGame() {
-    let humanScore = 0
-    let computerScore = 0
-    function playRound(humanChoice, computerChoice) {
 
-        if (humanChoice == "rock") {
-            switch (computerChoice) {
-                case "rock":
-                    console.log("Its a Draw " + computerChoice + " draws with " + humanChoice)
-                    break;
-                case "paper":
-                    console.log("You Lose " + computerChoice + " Beats " + humanChoice)
-                    computerScore += 1;
-                    break;
-                case "scissors":
-                    console.log("You Win " + humanChoice + " Beats " + computerChoice)
-                    humanScore += 1
-                    break;
-                default:
-                    break;
-            }
+playerChoiceButton = document.querySelectorAll(".pc")
+playerChoiceButton.forEach((btn) => {
+    btn.addEventListener('click', (e) => {
+        humanChoicee = e.target.textContent.toLowerCase()
+
+        function updatescores() {
+            document.querySelector(".humanScore").textContent = "You: " + humanScore 
+            document.querySelector(".computerScore").textContent = "AI: " + computerScore
+            document.querySelector(".result").textContent = result
         }
-        if (humanChoice == "paper") {
-            switch (computerChoice) {
-                case "rock":
-                    console.log("You Win " + humanChoice + " Beats " + computerChoice)
-                    humanScore += 1
-                    break;
-                case "paper":
-                    console.log("Its a Draw " + computerChoice + " draws with " + humanChoice)
-                    break;
-                case "scissors":
-                    console.log("You Lose " + computerChoice + " Beats " + humanChoice)
-                    computerScore += 1;
-                    break;
-                default:
-                    break;
-            }
+
+        playRound(humanChoicee, getComputerChoice())
+
+        if (humanScore == 5 && computerScore == 5) {
+            result = "Its a Draw"
+        } else if (humanScore == 5) {
+            result = "You Win"
+        } else if (computerScore == 5) {
+            result = "You Lose"
 
         }
-        if (humanChoice == "scissors") {
-            switch (computerChoice) {
-                case "rock":
-                    console.log("You Lose " + computerChoice + " Beats " + humanChoice)
-                    computerScore += 1;
-                    break;
-                case "paper":
-                    console.log("You Win " + humanChoice + " Beats " + computerChoice)
-                    humanScore += 1
-                    break;
-                case "scissors":
-                    console.log("Its a Draw " + computerChoice + " draws with " + humanChoice)
-                    break;
-                default:
-                    break;
-            }
-        }
-    }
-    for (let x = 0; x < 5; x++) {
-        playRound(getHumanChoice(), getComputerChoice())
-    }
-    if (humanScore > computerScore) {
-        console.log("You Win")
-    }else if (computerScore > humanScore) {
-        console.log("You Lose")
-    }else {
-        console.log("It's a Draw")
-    }
-}
 
-playGame()
+        if (humanScore >= 5 || computerScore >= 5) {
+            updatescores()
+            humanScore = 0
+            computerScore = 0
+        } else {
+            updatescores()
+        }
+
+
+
+    })
+})
